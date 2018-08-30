946dfe6b971bfcc2b66c77023dcdef550e286ca8
Unknown <andrew_khang_le@hotmail.com>
2018-08-30 15:38:20 -0500

    Click mouse to move

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,13 +2,14 @@ using System;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
-[RequireComponent(typeof (ThirdPersonCharacter))]
-public class PlayerMovement : MonoBehaviour
-{
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class PlayerMovement : MonoBehaviour{
+    [SerializeField] float walkMoveStopRadius = .2f;
+
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -21,10 +22,27 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            print("Cursor raycast hit" + cameraRaycaster);
-            currentClickTarget = cameraRaycaster.hit.point;  // So not set in default case
+            print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
+            switch (cameraRaycaster.layerHit)
+            {
+                case Layer.Walkable:
+                    currentClickTarget = cameraRaycaster.hit.point;  // So not set in default case
+                    break;
+                case Layer.Enemy:
+                    print("Not moving to enemy");
+                    break;
+                default:
+                    print("SHOULDN'T BE HERE");
+                    return;
+
+            }
         }
-        m_Character.Move(currentClickTarget - transform.position, false, false);
+        var playerToClickPoint = currentClickTarget - transform.position;
+        if (playerToClickPoint.magnitude >= walkMoveStopRadius) {
+            m_Character.Move(playerToClickPoint, false, false);
+        } else {
+            m_Character.Move(Vector3.zero, false, false);
+        }
     }
 }
 
